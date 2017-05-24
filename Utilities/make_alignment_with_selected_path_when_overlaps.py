340209a9df898fa4b8e76f8e5eef4a259ad56d05
Kinggerm <cndslr@foxmail.com>
2017-05-24 23:31:42 +0800

    batch processing

--- Utilities/make_alignment_with_selected_path_when_overlaps.py
+++ Utilities/make_alignment_with_selected_path_when_overlaps.py
@@ -8,18 +8,15 @@ from optparse import OptionParser
 
 
 def require_commands():
-    global options
-    usage = 'python '+str(os.path.basename(__file__))+' -f input.fasta'
+    usage = 'python '+str(os.path.basename(__file__))+' input_files'
     parser = OptionParser(usage=usage)
-    parser.add_option('-f', dest='in_fasta_file', help='followed by your input fasta file')
     parser.add_option('-k', dest='minimum_match', help='minimum continuous match above which as overlap. Default: 10', default=10, type=int)
     parser.add_option('-s', dest='similarity', help='similarity threshold above which took as overlap. Default: 0.8', default=0.8, type=float)
-    try:
-        (options, args) = parser.parse_args()
-    except Exception as e:
-        sys.stdout.write('\n\n######################################'+str(e))
-        sys.stdout.write('\n\n"-h" for more usage')
-        os._exit(0)
+    options, args = parser.parse_args()
+    if not args:
+        raise FileNotFoundError("No input fasta file found!")
+    else:
+        return options, args
 
 
 def read_fasta(fasta_dir):
@@ -133,17 +130,15 @@ def find_string_difference(this_string, this_reference, dynamic_span=2.0):
             return difference, proper_end
 
 
-def check_similarity_and_continuity(string1, string2):
-    global options
+def check_similarity_and_continuity(string1, string2, k_mer, similarity):
     overlap_len = len(string1)
     # continuity
-    k_mer = options.minimum_match
     words_string1 = set([string1[x:x+k_mer] for x in range(overlap_len-k_mer+1)])
     for i in range(overlap_len-k_mer+1):
         if string2[i:i+k_mer] in words_string1:
             return True
     # similarity
-    minimum_dif = (1-options.similarity)*overlap_len
+    minimum_dif = (1-similarity)*overlap_len
     if find_string_difference(string1, string2, math.ceil(minimum_dif))[0] > minimum_dif:
         return False
     else:
@@ -151,47 +146,52 @@ def check_similarity_and_continuity(string1, string2):
 
 
 def main():
-    require_commands()
-    global options
-    options.in_fasta_file = options.in_fasta_file.strip().strip('\'')
-    fasta_matrix = read_fasta(options.in_fasta_file)
-    first_seq = fasta_matrix[1][0]
-    new_len = len(first_seq) - (len(first_seq) - len(first_seq.replace('?', '')))*2
-    new_fasta_matrix = [[], [], 70]
-    count_contig = 1
-    i = 0
-    former_start = 0
-    if first_seq[0] == '?' or first_seq[-1] == '?':
-        sys.stdout.write('\nError: overlap cannot be in the two ends!')
-        os._exit(0)
-    while i < len(first_seq):
-        if first_seq[i] == '?':
-            this_start = i
-            while first_seq[i] == '?':
-                this_end = i
-                i += 1
-            this_overlap_len = this_end-this_start+1
-            if check_similarity_and_continuity(first_seq[this_start-this_overlap_len:this_start], first_seq[this_end+1:this_end+this_overlap_len+1]):
-                new_fasta_matrix[0].append('contig_'+str(count_contig))
-                new_fasta_matrix[1].append(former_start*'-'+first_seq[:this_start]+(new_len-this_start-former_start)*'-')
-                first_seq = first_seq[this_end+1:]
-                former_start += this_start - this_overlap_len
-                count_contig += 1
-                i = 0
-            else:
-                new_fasta_matrix[0].append('contig_'+str(count_contig))
-                new_fasta_matrix[1].append(former_start*'-'+first_seq[:this_start]+(new_len-this_start-former_start)*'-')
-                first_seq = first_seq[this_end+1:]
-                former_start += this_start - this_overlap_len
-                count_contig += 1
-                i = 0
-                sys.stdout.write('\nUnrecognized overlap at alignment!')  # ('+str(former_start+1)+'-'+str(former_start+this_overlap_len)+')!'
-                pass
+    options, argv = require_commands()
+    for in_fasta_file in argv:
+        if not os.path.exists(in_fasta_file):
+            raise FileExistsError(argv+" not found!")
         else:
-            i += 1
-    new_fasta_matrix[0].append('contig_'+str(count_contig))
-    new_fasta_matrix[1].append(former_start*'-'+first_seq)
-    write_fasta(options.in_fasta_file+'.contig_alignments.fasta', new_fasta_matrix, False)
+            fasta_matrix = read_fasta(in_fasta_file)
+            first_seq = fasta_matrix[1][0]
+            new_len = len(first_seq) - (len(first_seq) - len(first_seq.replace('?', '')))*2
+            new_fasta_matrix = [[], [], 70]
+            count_contig = 1
+            i = 0
+            former_start = 0
+            if first_seq[0] == '?' or first_seq[-1] == '?':
+                sys.stdout.write('\nError: overlap cannot be in the two ends!')
+                os._exit(0)
+            while i < len(first_seq):
+                if first_seq[i] == '?':
+                    this_start = i
+                    while first_seq[i] == '?':
+                        this_end = i
+                        i += 1
+                    this_overlap_len = this_end-this_start+1
+                    if check_similarity_and_continuity(first_seq[this_start-this_overlap_len:this_start],
+                                                       first_seq[this_end+1:this_end+this_overlap_len+1],
+                                                       k_mer=options.minimum_match,
+                                                       similarity=options.similarity):
+                        new_fasta_matrix[0].append('contig_'+str(count_contig))
+                        new_fasta_matrix[1].append(former_start*'-'+first_seq[:this_start]+(new_len-this_start-former_start)*'-')
+                        first_seq = first_seq[this_end+1:]
+                        former_start += this_start - this_overlap_len
+                        count_contig += 1
+                        i = 0
+                    else:
+                        new_fasta_matrix[0].append('contig_'+str(count_contig))
+                        new_fasta_matrix[1].append(former_start*'-'+first_seq[:this_start]+(new_len-this_start-former_start)*'-')
+                        first_seq = first_seq[this_end+1:]
+                        former_start += this_start - this_overlap_len
+                        count_contig += 1
+                        i = 0
+                        sys.stdout.write('\nUnrecognized overlap at alignment!')  # ('+str(former_start+1)+'-'+str(former_start+this_overlap_len)+')!'
+                        pass
+                else:
+                    i += 1
+            new_fasta_matrix[0].append('contig_'+str(count_contig))
+            new_fasta_matrix[1].append(former_start*'-'+first_seq)
+            write_fasta(in_fasta_file+'.contig_alignments.fasta', new_fasta_matrix, False)
 
 
 if __name__ == '__main__':

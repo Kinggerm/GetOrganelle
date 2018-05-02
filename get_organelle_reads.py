151b65d3681b29a24fc7c7109c27ec0b59d94b7e
Kinggerm <cndslr@foxmail.com>
2018-05-02 23:10:43 +0800

    version and license

--- get_organelle_reads.py
+++ get_organelle_reads.py
@@ -1170,7 +1170,7 @@ def timed_log(log, output_base):
 
 def main():
     time0 = time.time()
-    title = "\nGetOrganelle" \
+    title = "\nGetOrganelle v" + str(get_versions()) + \
             "\n" \
             "\nThis pipeline get organelle reads and genomes from genome skimming data by extending." \
             "\nFind updates in https://github.com/Kinggerm/GetOrganelle and see README.md for more information." \
@@ -1327,16 +1327,4 @@ def main():
 if __name__ == '__main__':
     main()
 
-"""Copyright 2016 Jianjun Jin
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License."""
+"""Copyright 2016 Jianjun Jin"""

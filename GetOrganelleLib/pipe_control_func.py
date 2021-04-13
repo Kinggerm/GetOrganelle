3718da18af40ee2ee84b593e24c4252cba17a8db
Kinggerm <cndslr@foxmail.com>
2021-04-13 15:47:17 +0800

    v1.7.4

--- GetOrganelleLib/pipe_control_func.py
+++ GetOrganelleLib/pipe_control_func.py
@@ -393,7 +393,7 @@ def map_with_bowtie2(seed_file, original_fq_files, bowtie_out, resume, threads, 
             else:
                 sys.stdout.write("\nError: " + output.decode("utf8") + "\n")
             if "has more quality values than read characters" in output.decode("utf8"):
-                raise Exception("Please check the integrity of your input reads!!")
+                raise Exception("Please check the integrity of your input reads (e.g. using md5)!!")
             else:
                 raise Exception("")
         elif "No such file" in output.decode("utf8") or "not found" in output.decode("utf8"):

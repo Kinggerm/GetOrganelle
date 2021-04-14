ea3c2c8cc9e2b22d4807224bc8976160832fe59e
Kinggerm <cndslr@foxmail.com>
2021-04-14 17:06:00 +0800

    v1.7.4

--- GetOrganelleLib/versions.py
+++ GetOrganelleLib/versions.py
@@ -17,6 +17,7 @@ versions = [
             "7. get_organelle_from_assembly.py: fix a bug with the malfunction of --continue when the input is gfa ",
             "8. get_organelle_from_reads.py/disentangle_organelle_assembly.py: correct typos",
             "9. pipe_control_func.py: map_with_bowtie2: warn reads integrity",
+            "10. get_organelle_config.py: verbose log for bowtie2 and blast",
         ],
         "time": "2021-04-09 14:00 UTC+8"
     },
